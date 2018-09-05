56dae3271f791ee07b02ac976ac8a11f9859ca5f
FR4NC15C0JH <jh.programador@gmail.com>
2018-09-05 16:30:43 -0300

    Refatoração validation e método create.

--- src/main/java/com/employees/profile/controller/UserController.java
+++ src/main/java/com/employees/profile/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.employees.profile.controller;
-import javax.jws.soap.SOAPBinding.Use;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employees.profile.enumerator.Profiles;
 import com.employees.profile.model.User;
 import com.employees.profile.service.UserService;
 import com.employees.profile.validation.UserValidation;
@@ -38,23 +41,27 @@ public class UserController {
 	@Autowired
 	private UserValidation userValidation;
 	
-	@RequestMapping("/create")
 	@PostMapping
-	//@PreAuthorize("hasAnyRole('ADMIN','CUSTOMER')")
+	@RequestMapping("/create")
 	public ResponseEntity<Response<User>> create(HttpServletRequest request, @RequestBody User user, BindingResult result){
 		Response<User> response = new Response<User>();
+		List<String> errors = new ArrayList<String>();
 		try {
-			userValidation.validateCreateUser(user, result);
+			userValidation.validadeSave(user, result);
 			if(result.hasErrors()) {
-				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				//result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				for(ObjectError error : result.getAllErrors()) {
+					errors.add(error.getDefaultMessage());
+				}
+				response.setErrors(errors);
 				return ResponseEntity.badRequest().body(response);
 			}
 			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			if(user.getProfiles() == null) {
+				user.setProfiles(Profiles.ROLE_CUSTOMER);
+			}
 			User userPersisted = (User) userService.createOrUpdate(user);
 			response.setData(userPersisted);
-		} catch (DuplicateKeyException dke) {
-			response.getErrors().add("Email already registered");
-			return ResponseEntity.badRequest().body(response);
 		} catch (Exception e) {
 			response.getErrors().add(e.getMessage());
 			return ResponseEntity.badRequest().body(response);
@@ -67,7 +74,6 @@ public class UserController {
 	public ResponseEntity<Response<User>> update(HttpServletRequest request, @RequestBody User user, BindingResult result){
 		Response<User> response = new Response<User>();
 		try {
-			userValidation.validateCreateUser(user, result);
 			if(result.hasErrors()) {
 				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
 				return ResponseEntity.badRequest().body(response);
--- src/main/java/com/employees/profile/validation/UserValidation.java
+++ src/main/java/com/employees/profile/validation/UserValidation.java
@@ -1,18 +1,36 @@
 package com.employees.profile.validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 
 import com.employees.profile.model.User;
+import com.employees.profile.service.UserService;
 
 @Component
 public class UserValidation {
+	
+	@Autowired
+	private UserService userService;
 	
 	public void validateCreateUser(User user, BindingResult result) {
 		if(user.getEmail() == null) {
 			result.addError(new ObjectError("User","Email no information"));
 			return;
 		}
+	}
+	
+	public void validadeUserEmail(User user,BindingResult result) {
+		User userSearch = this.userService.findByEmail(user.getEmail());
+		if(userSearch != null) {
+			result.addError(new ObjectError("User", "User cadastred!"));
+			return;
+		}
+	}
+	
+	public void validadeSave(User user,BindingResult result) {
+		this.validateCreateUser(user, result);
+		this.validadeUserEmail(user, result);
 	}
 }
